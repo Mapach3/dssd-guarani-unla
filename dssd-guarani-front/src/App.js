c239b76c905c5b785389fdee6bf9c327b28c571f
Luciano Otegui <lukapote56@live.com.mx>
2020-11-07 18:51:57 -0300

    Add Items Menu ADMIN

--- dssd-guarani-front/src/App.js
+++ dssd-guarani-front/src/App.js
@@ -1,8 +1,5 @@
 import React, {useState} from 'react';
 import {Login} from './components/Login/Login'
-import UserInsert from './components/UserInsert/index'
-import UserDrop from './components/UserDrop/index'
-import SubjectInsert from './components/SubjectInsert/SubjectInsert';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {Storage} from './components/Storage'
@@ -15,8 +12,6 @@ import {
 import HomeAdmin from './pages/HomeAdmin';
 import HomeStudent from './pages/HomeStudent';
 import HomeTeacher from './pages/HomeTeacher';
-import InsertInscriptionWindow from './components/InscriptionWindow/InsertInscriptionWindow';
-
 
 const App = () => {
 
@@ -40,10 +35,6 @@ const App = () => {
           <>
           <Redirect exact to="/login"/>
           <Login setToken={setUserToken} setImageUser={setImageUser} setRolUser={setRolUser} setNameUser={setNameUser}/>         
-          {/* <UserInsert />  */}
-          {/* <UserDrop /> */}
-          {/* <SubjectInsert /> */}
-          {/* <InsertInscriptionWindow /> */}
           </>
           : 
           <>
