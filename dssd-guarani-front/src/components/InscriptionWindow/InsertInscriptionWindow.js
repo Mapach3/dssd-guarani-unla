15f9a321afea81ce3954f99d08484c5d65667688
Dogui <londmt3@gmail.com>
2020-11-07 03:21:54 -0300

    advances on InsertInscriptionWindow

--- dssd-guarani-front/src/components/InscriptionWindow/InsertInscriptionWindow.js
+++ dssd-guarani-front/src/components/InscriptionWindow/InsertInscriptionWindow.js
@@ -5,22 +5,39 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid'
 import Chip from '@material-ui/core/Chip';
+import moment from 'moment'
 
 
 class InsertInscriptionWindow extends Component{
 
     state = {
-        
-        formStartDate : '',
-        formEndDate : '',
-        errorMsg : ''
+        errorMsg : '',
+        isLoading : false,
     }
 
+    handleSubmit = (ev) => {
+        debugger;
+        ev.preventDefault();
+        this.setState({isLoading : true})
+        
+        var startDate = new Date(moment(ev.target.startDate.value,"YYYY/MM/DD").toDate())
+        startDate.setHours(ev.target.startTime.value.split(":")[0])
+        startDate.setMinutes(ev.target.startTime.value.split(":")[1])
+        console.log(startDate)
+        
+        var endDate = new Date(moment(ev.target.startDate.value,"YYYY/MM/DD").toDate())
+        endDate.setHours(ev.target.endTime.split(":")[0])
+        endDate.setMinutes(ev.target.endTime.split(":")[1])
+        console.log(endDate)
+
+        //TODO: Validate Dates and Post Window.
+
+    }
 
     render(){
         return (
                 <Container maxWidth="xs">
-                    <h3>Ingrese datos de la materia</h3>
+                    <h3>Nueva ventana de inscripción</h3>
                     {this.state.errorMsg.length !== 0 ?
                                 <>
                                 <Chip
@@ -33,21 +50,23 @@ class InsertInscriptionWindow extends Component{
                                 <br /><br />
                                 </> : 
                             null}
-                    <form autoComplete="off">
-                        <Grid container spacing={1}>
+                    <form autoComplete="off" onSubmit={this.handleSubmit}>
+                        <Grid container spacing={2}>
                         <Grid item sm={6}>
-                            <TextField id="startDate" label="Inicio" type="date" />
-                            <TextField fullWidth id="time" variant="outlined" label="Hora de Inicio"  type="time"/>
+                            <TextField fullWidth variant="outlined" id="startDate" label="Dia de Inicio" type="date" InputLabelProps={{ shrink: true }} />
                         </Grid>
-                            
-                            
                         <Grid item sm={6}>
-                        <TextField id="endDate" label="Fin" type="date" />
-                            <TextField fullWidth id="time" variant="outlined" label="Hora de Fin" type="time"/>
+                            <TextField fullWidth id="startTime" variant="outlined" label="Hora de Inicio"  type="time" InputLabelProps={{ shrink: true }} />
+                        </Grid>       
+                        <Grid item sm={6}>
+                            <TextField fullWidth variant="outlined" id="endDate" label="Dia de Fin" type="date" InputLabelProps={{ shrink: true }} />
+                        </Grid>
+                        <Grid item sm={6}>
+                            <TextField fullWidth id="endTime" variant="outlined" label="Hora de Fin" type="time" InputLabelProps={{ shrink: true }}/>
                         </Grid>
                         </Grid>
                         <br />
-                        <Button variant="contained"  color="primary">
+                        <Button variant="contained" disabled={this.state.isLoading} type="submit" color="primary">
                             Agregar Ventana
                         </Button>                        
                     </form>
