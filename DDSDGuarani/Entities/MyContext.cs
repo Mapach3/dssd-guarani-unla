cc1a924ca923eb95802f7d53b5446649f04b7ccb
Tomás Pereyra <tpereyra@danaide.com.ar>
2020-11-10 23:29:22 -0300

    Agrego datos

--- DDSDGuarani/Entities/MyContext.cs
+++ DDSDGuarani/Entities/MyContext.cs
@@ -68,10 +68,107 @@ namespace DDSDGuarani.Entities
                     PostalCode="1234",
                     City="1234",
                     Country="Argentina"
+                },
+                 new Address
+                {
+                    Id=2,
+                    StreetAndNumber="Calle 123",
+                    Location="Lanus",
+                    PostalCode="1234",
+                    City="1234",
+                    Country="Argentina"
                 }
               
                );
+            modelBuilder.Entity<User>().HasData(
+                new User{
+                    Id=1,
+                    Email="pereyratomas18@gmail.com",
+                    Password="1234",
+                    Name="Tomas",
+                    Surname="Pereyra",
+                    Dni="40897248",
+                    Active=true,
+                    PasswordChanged=true,
+                    ImgBase64=null,
+                    Role=Enums.UserRole.TEACHER,
+                    AddressId=1
 
+                },
+                new User{
+                    Id=2,
+                    Email="pereyratomas18@gmail.com",
+                    Password="1234",
+                    Name="Juan",
+                    Surname="Manuel",
+                    Dni="11111111",
+                    Active=true,
+                    PasswordChanged=true,
+                    ImgBase64=null,
+                    Role=Enums.UserRole.TEACHER,
+                    AddressId=2
+
+                }
+            );
+              modelBuilder.Entity<InscriptionWindow>().HasData(
+                  new InscriptionWindow{
+                      Id=1,
+                      StartDate=  new DateTime(2020, 6, 10),
+                      EndDate = new DateTime(2020, 9, 10),                  
+
+                  },
+                  new InscriptionWindow{
+                      Id=2,
+                      StartDate=  new DateTime(2020, 6, 15),
+                      EndDate = new DateTime(2020, 9, 15),                  
+
+                  }
+              );
+
+              modelBuilder.Entity<Subject>().HasData(
+                  new Subject{
+                      Id=1,
+                      Name="Matematica 1",
+                      StartTime =new DateTime(2020, 6, 25),
+                      EndTime=new DateTime(2020, 8, 25),
+                      ScoreUploadLimit=new DateTime(2020, 8, 20),
+                      Year=2020,
+                      Period=1,
+                      Shift=Enums.SubjectShift.AFTERNOON,
+                      InscriptionWindowId=1
+
+
+
+                  },
+                  new Subject{
+                      Id=2,
+                      Name="Matematica 2",
+                      StartTime =new DateTime(2020, 6, 25),
+                      EndTime=new DateTime(2020, 8, 25),
+                      ScoreUploadLimit=new DateTime(2020, 8, 20),
+                      Year=2020,
+                      Period=2,
+                      Shift=Enums.SubjectShift.EVENING,
+                      InscriptionWindowId=1
+
+
+
+                  }
+                   
+              );
+
+               modelBuilder.Entity<Course>().HasData(
+                   new Course{
+                    SubjectId=1,
+                    UserId=1,
+                    CourseAverage=0
+                   },
+                   new Course{
+                    SubjectId=2,
+                    UserId=1,
+                    CourseAverage=0
+                   }
+               );
 
         
         }
