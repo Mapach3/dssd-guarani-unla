5e2145544e4ed0e4741f02a38afdf8a2402f7c88
giannativo <giannativo@yahoo.com.ar>
2020-11-16 22:28:04 -0300

    adding final inscription

---
+++ dssd-guarani-front/src/components/StudentInscription/FinalInscription/SubjectGrid.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button'
+import './style.css'
+
+class finalGrid extends Component{
+
+    sendUserData(ev,finalId,active,inWindow){
+        debugger;
+        var userId = ev.currentTarget.value
+        ev.currentTarget.disabled = true
+        this.props.action(userId,finalId,active,inWindow)
+        ev.currentTarget.disabled = false
+    }
+
+    isWindowActive(final){
+        var startDate = final.inscriptionWindow.startDate;
+        var endDate = final.inscriptionWindow.endDate;
+        var currentDate = new Date(Date.now()).toISOString();
+        console.log(currentDate < endDate && currentDate > startDate)
+        return (currentDate < endDate && currentDate > startDate)
+    }
+
+    isUserInfinal(final){
+        return final.inscriptionFinals.find(inscription => inscription.userId == window.localStorage.getItem('userId'))
+    }
+
+    getSubject(final, subjectsList){
+        return subjectsList.find(subject => subject.id == final.subject).name
+    }
+
+    removePreviousFinals(finals){
+        var currentDate = new Date(Date.now()).toISOString();
+        return finals.filter(final => final.date > currentDate)
+    }
+
+    render(){
+        const {finals, subjectsList} = this.props
+        return <>
+        <TableContainer className="userDropTables" component={Paper}>
+            <Table aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                    <TableCell># ID</TableCell>
+                    <TableCell align="left">Nombre</TableCell>
+                    <TableCell align="left">Fecha</TableCell>
+                    <TableCell align="left">Activo</TableCell>
+                    <TableCell align="left">Acción</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {this.removePreviousFinals(finals).map((final) => (
+                    <TableRow key={final.id}>
+                        <TableCell component="th" scope="row">{final.id}</TableCell>
+                        <TableCell align="left">{this.getSubject(final, subjectsList)}</TableCell>
+                        <TableCell align="left">{final.date}</TableCell>
+                        <TableCell align="left">{this.isUserInfinal(final) ? "Si" : "No"}</TableCell>
+                        <TableCell align="left">
+                        <Button variant="contained" value={window.localStorage.getItem('userId')} disabled={false} 
+                        color={this.isUserInfinal(final) ? "secondary" : "primary"} onClick={(ev) => this.sendUserData(ev, final.id, this.isUserInfinal(final), this.isWindowActive(final))}>{this.isUserInfinal(final) ? "Baja" : "Alta"}</Button>      
+                        </TableCell>
+                    </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+      </TableContainer>
+      </>
+
+
+
+        
+    }
+
+
+
+
+
+
+}
+
+export default finalGrid
---
+++ dssd-guarani-front/src/components/StudentInscription/FinalInscription/index.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import { __API_SUBJECTSTUDENT, __API_USERSTUDENT, __API_COURSESTUDENT, __API_FINALSTUDENT, __API_FINAL } from '../../../consts/consts';
+import clsx from 'clsx';
+import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress'
+import SubjectGrid from './SubjectGrid';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
+
+class FinalInscription extends Component {
+
+  state = {
+    finalsList: [],
+    subjectsList: [],
+    approvedFinalsCodes: [],
+    loading: true,
+    errorMsg: '',
+    dialogOpen: false
+  }
+
+  componentDidMount() {
+    axios.get(__API_SUBJECTSTUDENT).then(resp => {
+      console.log(resp.data);
+      this.setState({
+        subjectsList: resp.data
+      })
+      this.updateFinalsList(resp.data)
+    })
+  }
+
+  getNotApprovedFinals(finals, subjects) {
+    this.approvedFinalsCodes = this.getApprovedFinalCodes(finals)
+    console.log(finals.filter(final => !this.isAnApprovedCode(final.subjectCode) && final.inscriptionWindow.id == 2 && this.getCareerIdOfSubject(final, subjects) == window.localStorage.getItem('careerId')))
+    return finals.filter(final => !this.isAnApprovedCode(final.subjectCode) && final.inscriptionWindow.id == 2 && this.getCareerIdOfSubject(final, subjects) == window.localStorage.getItem('careerId'))
+  }
+
+  getCareerIdOfSubject(final, subjects) {
+    console.log(subjects)
+    return subjects.find(subject => subject.id == final.subject).career.id
+  }
+
+  isAnApprovedCode(code) {
+    return this.approvedFinalsCodes.find(subjectCode => subjectCode == code)
+  }
+
+  finalIsApproved(finals) {
+    return finals.find(final => final.userId == window.localStorage.getItem('userId') && Number(final.score) >= 4)
+  }
+
+  getApprovedFinalCodes(finals) {
+    var codes = []
+    finals.map((final) => {
+      if(this.finalIsApproved(final.inscriptionFinals)){
+        codes.push(this.subjectsList.find(subject => subject.id == final.subjectId).subjectCode)
+      }
+    });
+    console.log(codes);
+    return codes;
+  }
+
+  updateFinalsList(subjects) {
+    axios.get(__API_FINALSTUDENT).then(resp => {
+      console.log(resp.data);
+      this.setState({
+        finalsList: this.getNotApprovedFinals(resp.data, subjects),
+        loading: false
+      })
+    })
+  }
+
+  handleDialog() {
+    this.setState({ dialogOpen: !this.state.dialogOpen })
+  }
+
+  createInscription(userId,finalId,active,inWindow) {
+    this.setState({ errorMsg: '' })
+    var id = parseInt(userId)
+    var method = "POST"
+    if(active){
+      method = "DELETE"
+    }
+    if(inWindow){
+      const options = {
+        method : method,
+        url : __API_FINAL,
+      
+        data : {
+            userid : Number(userId),
+            finalid : Number(finalId)
+        }
+      } 
+      axios(options).then(resp => {
+        this.setState({errorMsg : "Inscripcion a final exitosa.", dialogOpen : true})
+        this.componentDidMount()
+      })
+    }
+    else{
+      this.setState({errorMsg : "La ventana de inscripcion no se encuentra activa, operacion no valida.", dialogOpen : true})
+      this.componentDidMount()
+    }
+  }
+
+  render() {
+    const { finalsList, subjectsList, loading, errorMsg } = this.state
+    return (
+      // <main
+      //   className={clsx(this.props.classes.content, {
+      //     [this.props.classes.contentShift]: this.props.open,
+      //   })}
+      // >
+        // <div className={this.props.classes.drawerHeader} />
+        <Container maxWidth="md">
+          <h3>Listado de finales</h3>
+          <SubjectGrid finals={finalsList} subjectsList={subjectsList} action={(userId,finalId,active,inWindow) => this.createInscription(userId,finalId,active,inWindow)}/>
+          <Dialog
+            open={this.state.dialogOpen}
+            keepMounted
+            onClose={() => this.handleDialog()}
+          >
+            <DialogContent>
+              <DialogContentText id="alert-dialog-slide-description">
+                {errorMsg}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => this.handleDialog()} color="primary">
+                Aceptar
+            </Button>
+            </DialogActions>
+          </Dialog>
+        </Container>
+      // </main>
+    )
+  }
+
+}
+
+export default FinalInscription
